8b14c37d95d4923e2a8060e8e25e50dfdea8be79
aaron toliver <flux.charlotte@gmail.com>
2019-09-04 17:01:28 -0400

    stream delete

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -13,8 +13,16 @@ class StreamList extends React.Component {
 			return (
 				<div>
 					<div className='right floated content'>
-						<button className='ui button primary'>Edit</button>
-						<button className='ui button negative'>Delete</button>
+						<Link
+							to={`/streams/edit/${stream.id}`}
+							className='ui button primary'>
+							Edit
+						</Link>
+						<Link
+							to={`/streams/delete/${stream.id}`}
+							className='ui button negative'>
+							Delete
+						</Link>
 					</div>
 				</div>
 			);
@@ -28,7 +36,10 @@ class StreamList extends React.Component {
 					{this.renderAdmin(stream)}
 					<i className='large middle aligned icon camera' />
 					<div className='content'>
-						{stream.title}
+						<Link to={`/stresms/${stream.id}`} className='header'>
+							{stream.title}
+						</Link>
+
 						<div className='description'>{stream.description}</div>
 					</div>
 				</div>
